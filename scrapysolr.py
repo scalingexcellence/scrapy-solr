10e56834e2b30f8566fd1c1c27372db372308bb6
German <germans@gmail.com>
2016-04-30 18:33:47 +0100

    fixed indent and missing import

--- scrapysolr.py
+++ scrapysolr.py
@@ -15,6 +15,7 @@
 """Solr Pipeline for scrapy"""
 
 import pysolr
+import logging
 from scrapy.conf import settings
 
 
@@ -27,28 +28,26 @@ class SolrPipeline(object):
             raise RuntimeError('To ignore duplicates SOLR_DUPLICATES_KEY_FIELDS has to be defined')
         self.solr = pysolr.Solr(settings['SOLR_URL'], timeout=10)
 
+    def process_item(self, item, spider):
+        if self.ignore_duplicates:
+            dup_keys_values = [str(dst) + ':' + '"' + self.__get_item_value__(item, src) + '"' for dst, src in
+                               self.mapping
+                               if dst in self.id_fields]
+            query = " ".join(dup_keys_values)
+            result = self.solr.search(query)
+            if len(result) != 0:
+                logging.info('Skipping duplicate')
+                return item
+        solr_item = {}
+        for dst, src in self.mapping:
+            solr_item[dst] = self.__get_item_value__(item, src)
+        self.solr.add([solr_item])
+        return item
 
-def process_item(self, item, spider):
-    if self.ignore_duplicates:
-        dup_keys_values = [str(dst) + ':' + '"' + self.__get_item_value__(item, src) + '"' for dst, src in
-                           self.mapping
-                           if dst in self.id_fields]
-        query = " ".join(dup_keys_values)
-        result = self.solr.search(query)
-        if len(result) != 0:
-            logging.info('Skipping duplicate')
-            return item
-    solr_item = {}
-    for dst, src in self.mapping:
-        solr_item[dst] = self.__get_item_value__(item, src)
-    self.solr.add([solr_item])
-    return item
-
-
-def __get_item_value__(self, item, src):
-    if type(src) is str:
-        return item[src] if src in item else None
-    elif type(src) is list:
-        return [item[i] if i in item else None for i in src]
-    else:
-        raise TypeError('Only string and list are valid mapping source')
+    def __get_item_value__(self, item, src):
+        if type(src) is str:
+            return item[src] if src in item else None
+        elif type(src) is list:
+            return [item[i] if i in item else None for i in src]
+        else:
+            raise TypeError('Only string and list are valid mapping source')
